a82b212a23419e83316927e6ce28fb5678c51164
Jessica Santos <jel_008@hotmail.com>
2026-04-14 14:48:24 -0300

    First commit: Upload Project

---
+++ src/main/java/com/jessicasantos89/product_api/ProductApiApplication.java
@@ -0,0 +1,13 @@
+package com.jessicasantos89.product_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jessicasantos89/product_api/ProductApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jessicasantos89.product_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
