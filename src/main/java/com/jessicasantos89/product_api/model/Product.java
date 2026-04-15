65d8629ea5eef3baad04762a723dfce1cf16714b
Jessica Santos <jel_008@hotmail.com>
2026-04-15 13:51:20 -0300

    Creating Requests

--- src/main/java/com/jessicasantos89/product_api/model/Product.java
+++ src/main/java/com/jessicasantos89/product_api/model/Product.java
@@ -1,14 +1,22 @@
 package com.jessicasantos89.product_api.model;
 
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
 public class Product {
-    private int id;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    @Column
     private String name;
+    @Column
     private String description;
+    @Column
     private Double price;
 
     public Product() {
